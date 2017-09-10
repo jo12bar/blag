4bd630a8264f2364d248f89bb2330f333ec6de8f
jo12bar <johann.b@telus.net>
2017-09-09 18:54:22 -0700

    Remove console.log debugging in isLoading selector

--- src/selectors/isLoading.js
+++ src/selectors/isLoading.js
@@ -8,11 +8,6 @@ export default createSelector(
     state => state.videosByCategory,
   ],
   (type, { slug, category }, videosHash, videosByCategory) => {
-    console.log('type:', type);
-    console.log('slug:', slug);
-    console.log('category:', category);
-    console.log('videosHash:', videosHash);
-    console.log('videosByCategory:', videosByCategory);
     if (type === 'VIDEO') return typeof videosHash[slug] === 'undefined';
     if (type === 'LIST') return !videosByCategory[category];
     return false;
